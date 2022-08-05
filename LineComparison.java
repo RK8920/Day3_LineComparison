ce4f89e3ebc2b5ab55dee00346c9cd2f7fd05d17
Rohit Kamble <kamble.rohit10792@gmail.com>
2022-08-05 23:17:57 +0530

    Added:Functionality to check both lines are equal or not Using Equals Method

--- LineComparison.java
+++ LineComparison.java
@@ -7,6 +7,7 @@ public class LineComparison {
     public static void main(String[] args) {
 
         Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter the Values for First Line");
         System.out.println("Enter the Values of the points X1 and Y1");
         System.out.println("Enter the value of X1 :  ");
         int X1 = Scan.nextInt();
@@ -17,8 +18,28 @@ public class LineComparison {
         int X2 = Scan.nextInt();
         System.out.println("Enter the value of Y2 :  ");
         int Y2 = Scan.nextInt();
-        Double lengthOfLine1 = Math.sqrt((X2 - X1) * (Y2 - Y1) + (Y2 - Y1) * (Y2 - Y1));
-        System.out.println("The length of the line is  : " + lengthOfLine1);
 
+        Double lengthOfLine1 = Math.sqrt((X2 - X1) * (Y2 - Y1) + (Y2 - Y1) * (Y2 - Y1));
+        System.out.println("The length of First line is  : " + lengthOfLine1);
+
+        System.out.println("Enter the Values for Second Line");
+        System.out.println("Enter the Values of the line X3 and Y3");
+        System.out.println("Enter the value of X3 :  ");
+        int X3 = Scan.nextInt();
+        System.out.println("Enter the value of Y3 :  ");
+        int Y3 = Scan.nextInt();
+        System.out.println("Enter the Values of the line X4 and Y4");
+        System.out.println("Enter the value of X4 :  ");
+        int X4 = Scan.nextInt();
+        System.out.println("Enter the value of Y4 :  ");
+        int Y4 = Scan.nextInt();
+
+        Double lengthOfLine2 = Math.sqrt((X4 - X3) * (Y4 - Y3) + (Y4 - Y3) * (Y4 - Y3));
+        System.out.println("The length of Second line is  : " + lengthOfLine2);
+
+        if (lengthOfLine1.equals(lengthOfLine2)) {
+            System.out.println("Both Lines are Equal");
+        } else
+            System.out.println("Both Lines are Not Equal");
     }
 }
